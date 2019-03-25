925980cfb66c5401d7aa2d9dc9184448f00f45a6
Nguyen Hang <nov@NH.local>
2019-03-24 21:18:59 -0400

    Completed tabLayout

--- app/src/main/java/com/example/mytour/MainActivity.java
+++ app/src/main/java/com/example/mytour/MainActivity.java
@@ -1,14 +1,15 @@
 package com.example.mytour;
 
+import android.support.design.widget.TabItem;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 
 public class MainActivity extends AppCompatActivity {
-
 
 
     @Override
@@ -26,17 +27,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Find the view pager that will allow the user to swipe between fragments
-        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        ViewPager viewPager = findViewById(R.id.viewpager);
 
         // Create an adapter that knows which fragment should be shown on each page
-        CategoryAdapter adapter = new CategoryAdapter(getSupportFragmentManager());
+        CategoryAdapter adapter = new CategoryAdapter(this, getSupportFragmentManager());
 
         // Set the adapter onto the view pager
         viewPager.setAdapter(adapter);
         //Show tab
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.toolbar);
-        //tabLayout.setupWithViewPager(viewPager);
-
+        TabLayout tabLayout = findViewById(R.id.tablayout);
+        tabLayout.setupWithViewPager(viewPager);
 
     }
 }
